3890c2859525ecd4721aec4b8fb3e8e4a6421eec
Harry <harish1090prasad@gmail.com>
2024-08-29 08:33:42 +0530

    new updates

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,11 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import myImage from './assets/profile.jpg';
 
-
-
 export default function Home() {
-
-
   
   const [text, setText] = useState(' HELLO ');
 
@@ -18,46 +14,37 @@ export default function Home() {
     return () => clearInterval(intervalId);
   }, []);
 
-
-
-
-  
-
-
   return (
     <>
 
+        <section id="home" class="section"  >
+            <div class="section__container">
+            
+              <div class="content" >
+                <p class="subtitle">hey</p>
+                <h1 class="title">
+                  I'm <span>Harish Prasad<br/>a</span> Tech Enthusiast
+                </h1>
+                <p class="description"> <i class="fa-solid fa-wand-magic-sparkles"></i>
+                
+                  Welcome to my Tech Stack portfolio! I'm Harish Prasad, a skilled and
+                  creative Tech Enthusiast with a passion for creating beautiful,
+                  responsive, and user-friendly websites. I've worked on a variety of
+                  web projects, ranging from personal blogs to e-commerce platforms.
 
-<section id="home" class="section"  >
-    <div class="section__container">
-     
-      <div class="content" >
-        <p class="subtitle">hey</p>
-        <h1 class="title">
-          I'm <span>Harish Prasad<br/>a</span> Tech Enthusiast
-        </h1>
-        <p class="description"> <i class="fa-solid fa-wand-magic-sparkles"></i>
-        
-          Welcome to my Tech Stack portfolio! I'm Harish Prasad, a skilled and
-          creative Tech Enthusiast with a passion for creating beautiful,
-          responsive, and user-friendly websites. I've worked on a variety of
-          web projects, ranging from personal blogs to e-commerce platforms.
+                </p>
+                <div class="action__btns">
+                  <button class="hire__me"><i class="fa-solid fa-bolt fa-fade"></i>Hire Me </button>
+                  <button class="portfolio">Portfolio</button>
+                </div>
+              </div>
 
-        </p>
-        <div class="action__btns">
-          <button class="hire__me"><i class="fa-solid fa-bolt fa-fade"></i>Hire Me </button>
-          <button class="portfolio">Portfolio</button>
-        </div>
-      </div>
-
-      <div class="image">
-        <img src={myImage} alt="profile" />
-      </div>
-     
-    </div>
-  </section>
-    
-    
+              <div class="image">
+                <img src={myImage} alt="profile" />
+              </div>
+            
+            </div>
+          </section>
     </>
 
   )
--- src/components/Project/Project.jsx
+++ src/components/Project/Project.jsx
@@ -10,161 +10,161 @@ import img4 from  './assets/storyfasion.JPG';
 export default function Project() {
   return (
    <>
-   
-<div class="Head" id="projects">
-  <div class="Head1">
+            
+            <div class="Head" id="projects">
+            <div class="Head1">
 
-  <h1 class="myprojecth">My Projects</h1>
+            <h1 class="myprojecth">My Projects</h1>
 
-  </div>
-  <div class="Head2">
+            </div>
+            <div class="Head2">
 
-  <p class="myprojectp" ><i class="fa-brands fa-wpexplorer"></i> Explore some of my recent work.</p>
+            <p class="myprojectp" ><i class="fa-brands fa-wpexplorer"></i> Explore some of my recent work.</p>
 
-  </div>
-</div>
-
-
-<div class="Pro">
-    <div class="UniqueProjects" >
-
-    <div class="UniqueCard">
-
-        <div class="UniqueProfilePic">
-        <img src={img3} alt="https://wall.alphacoders.com/big.php?i=1334174"/>
-        </div>
-
-        <div class="UniqueDetails">
-            <div class="UniqueContent">
-                <h2>SPACE-C</h2>
-                <p class="UniqueTag">TITAN CITY</p>
-                <p class="UniqueAbout">  We're thrilled to present Space Cyphers as our entry for the NASA Space Apps Challenge 2023.</p>
+            </div>
             </div>
 
-            <div class="UniqueButtons">
-                <div class="UniqueSocial">
-                <a href="https://github.com/HarishTPrasad/Space-Cyphers" >
-        <i class="fa-brands fa-instagram"></i>
-        </a>
-        <a href="https://github.com/HarishTPrasad/Space-Cyphers" >
-        <i class="fa-brands fa-twitter"></i>
-        </a>
-        <a href="https://github.com/HarishTPrasad/Space-Cyphers" >
-        <i class="fa-brands fa-github"></i>
-        </a>
-    
+
+            <div class="Pro">
+                <div class="UniqueProjects" >
+
+                <div class="UniqueCard">
+
+                    <div class="UniqueProfilePic">
+                    <img src={img3} alt="https://wall.alphacoders.com/big.php?i=1334174"/>
+                    </div>
+
+                    <div class="UniqueDetails">
+                        <div class="UniqueContent">
+                            <h2>SPACE-C</h2>
+                            <p class="UniqueTag">TITAN CITY</p>
+                            <p class="UniqueAbout">  We're thrilled to present Space Cyphers as our entry for the NASA Space Apps Challenge 2023.</p>
+                        </div>
+
+                        <div class="UniqueButtons">
+                            <div class="UniqueSocial">
+                            <a href="https://github.com/HarishTPrasad/Space-Cyphers" >
+                    <i class="fa-brands fa-instagram"></i>
+                    </a>
+                    <a href="https://github.com/HarishTPrasad/Space-Cyphers" >
+                    <i class="fa-brands fa-twitter"></i>
+                    </a>
+                    <a href="https://github.com/HarishTPrasad/Space-Cyphers" >
+                    <i class="fa-brands fa-github"></i>
+                    </a>
+                
+                            </div>
+                            <a href="https://space-cyphers.vercel.app/" class="link">project <i class="fa-solid fa-up-right-from-square"></i></a>
+                        </div>
+
+                    </div>
+
                 </div>
-                <a href="https://space-cyphers.vercel.app/" class="link">project <i class="fa-solid fa-up-right-from-square"></i></a>
+
+                <div class="UniqueCard">
+
+            <div class="UniqueProfilePic">
+            <img src={img2} alt="https://wall.alphacoders.com/big.php?i=1334174"/>
             </div>
 
-        </div>
+            <div class="UniqueDetails">
+                <div class="UniqueContent">
+                    <h2>SHESHIELD</h2>
+                    <p class="UniqueTag">SOS AAP</p>
+                    <p class="UniqueAbout">Empowering women's safety with quick triggers, location sharing, and multimedia alerts.</p>
+                </div>
 
-    </div>
+                <div class="UniqueButtons">
+                    <div class="UniqueSocial">
+                    <a href="https://github.com/HarishTPrasad/SHE_SHIELD" >
+                    <i class="fa-brands fa-instagram"></i>
+                    </a>
+                    <a href="https://github.com/HarishTPrasad/SHE_SHIELD" >
+                    <i class="fa-brands fa-twitter"></i>
+                    </a>
+                    <a href="https://github.com/HarishTPrasad/SHE_SHIELD" >
+                    <i class="fa-brands fa-github"></i>
+                    </a>
+                
+                
+            
+                    </div>
+                    <a href="https://github.com/HarishTPrasad/Space-Cyphers" class="link">project <i class="fa-solid fa-up-right-from-square"></i></a>
+                </div>
 
-    <div class="UniqueCard">
+            </div>
 
-<div class="UniqueProfilePic">
-<img src={img2} alt="https://wall.alphacoders.com/big.php?i=1334174"/>
-</div>
+            </div>
 
-<div class="UniqueDetails">
-    <div class="UniqueContent">
-        <h2>SHESHIELD</h2>
-        <p class="UniqueTag">SOS AAP</p>
-        <p class="UniqueAbout">Empowering women's safety with quick triggers, location sharing, and multimedia alerts.</p>
-    </div>
+            <div class="UniqueCard">
 
-    <div class="UniqueButtons">
-        <div class="UniqueSocial">
-        <a href="https://github.com/HarishTPrasad/SHE_SHIELD" >
-        <i class="fa-brands fa-instagram"></i>
-        </a>
-        <a href="https://github.com/HarishTPrasad/SHE_SHIELD" >
-        <i class="fa-brands fa-twitter"></i>
-        </a>
-        <a href="https://github.com/HarishTPrasad/SHE_SHIELD" >
-        <i class="fa-brands fa-github"></i>
-        </a>
-    
-    
-  
-        </div>
-        <a href="https://github.com/HarishTPrasad/Space-Cyphers" class="link">project <i class="fa-solid fa-up-right-from-square"></i></a>
-    </div>
+            <div class="UniqueProfilePic">
+            <img src={img4} alt="https://wall.alphacoders.com/big.php?i=1334174"/>
+            </div>
 
-</div>
+            <div class="UniqueDetails">
+                <div class="UniqueContent">
+                    <h2>STORY-F</h2>
+                    <p class="UniqueTag">LOCAL STORE</p>
+                    <p class="UniqueAbout"> Live webpage for a local Fasion store in Mumbai that wants to spread there reach digitally </p>
+                </div>
 
-</div>
+                <div class="UniqueButtons">
+                    <div class="UniqueSocial">
+                    
+                    <a href="https://github.com/HarishTPrasad/StoryFashion" >
+                    <i class="fa-brands fa-instagram"></i>
+                    </a>
+                    <a href="https://github.com/HarishTPrasad/StoryFashion" >
+                    <i class="fa-brands fa-twitter"></i>
+                    </a>
+                    <a href="https://github.com/HarishTPrasad/StoryFashion" >
+                    <i class="fa-brands fa-github"></i>
+                    </a>
+                
+                    </div>
+                    <a href="https://story-fashion.vercel.app/" class="link">project <i class="fa-solid fa-up-right-from-square"></i></a>
+                </div>
 
-<div class="UniqueCard">
+            </div>
 
-<div class="UniqueProfilePic">
-<img src={img4} alt="https://wall.alphacoders.com/big.php?i=1334174"/>
-</div>
+            </div>
 
-<div class="UniqueDetails">
-    <div class="UniqueContent">
-        <h2>STORY-F</h2>
-        <p class="UniqueTag">LOCAL STORE</p>
-        <p class="UniqueAbout"> Live webpage for a local Fasion store in Mumbai that wants to spread there reach digitally </p>
-    </div>
+            <div class="UniqueCard">
 
-    <div class="UniqueButtons">
-        <div class="UniqueSocial">
-        
-        <a href="https://github.com/HarishTPrasad/StoryFashion" >
-        <i class="fa-brands fa-instagram"></i>
-        </a>
-        <a href="https://github.com/HarishTPrasad/StoryFashion" >
-        <i class="fa-brands fa-twitter"></i>
-        </a>
-        <a href="https://github.com/HarishTPrasad/StoryFashion" >
-        <i class="fa-brands fa-github"></i>
-        </a>
-    
-        </div>
-        <a href="https://story-fashion.vercel.app/" class="link">project <i class="fa-solid fa-up-right-from-square"></i></a>
-    </div>
+            <div class="UniqueProfilePic">
+            <img src={img1} alt="https://wall.alphacoders.com/big.php?i=1334174"/>
+            </div>
 
-</div>
+            <div class="UniqueDetails">
+                <div class="UniqueContent">
+                    <h2>ATG-W</h2>
+                    <p class="UniqueTag">WEB PAGE</p>
+                    <p class="UniqueAbout">Fully responsive professional webpage that has frontend with reponsive login and sign up pop-ups.</p>
+                </div>
 
-</div>
+                <div class="UniqueButtons">
+                    <div class="UniqueSocial">
+                    <a href="https://github.com/HarishTPrasad/ATG-World" >
+                    <i class="fa-brands fa-instagram"></i>
+                    </a>
+                    <a href="https://github.com/HarishTPrasad/ATG-World" >
+                    <i class="fa-brands fa-twitter"></i>
+                    </a>
+                    <a href="https://github.com/HarishTPrasad/ATG-World" >
+                    <i class="fa-brands fa-github"></i>
+                    </a>
+                
+                    </div>
+                    <a href="https://atg-worldharry.vercel.app/" class="link">project <i class="fa-solid fa-up-right-from-square"></i></a>
+                </div>
 
-<div class="UniqueCard">
+            </div>
 
-<div class="UniqueProfilePic">
-<img src={img1} alt="https://wall.alphacoders.com/big.php?i=1334174"/>
-</div>
+            </div> 
 
-<div class="UniqueDetails">
-    <div class="UniqueContent">
-        <h2>ATG-W</h2>
-        <p class="UniqueTag">WEB PAGE</p>
-        <p class="UniqueAbout">Fully responsive professional webpage that has frontend with reponsive login and sign up pop-ups.</p>
-    </div>
-
-    <div class="UniqueButtons">
-        <div class="UniqueSocial">
-        <a href="https://github.com/HarishTPrasad/ATG-World" >
-        <i class="fa-brands fa-instagram"></i>
-        </a>
-        <a href="https://github.com/HarishTPrasad/ATG-World" >
-        <i class="fa-brands fa-twitter"></i>
-        </a>
-        <a href="https://github.com/HarishTPrasad/ATG-World" >
-        <i class="fa-brands fa-github"></i>
-        </a>
-    
-        </div>
-        <a href="https://atg-worldharry.vercel.app/" class="link">project <i class="fa-solid fa-up-right-from-square"></i></a>
-    </div>
-
-</div>
-
-</div> 
-
-</div>
-</div>
+            </div>
+            </div>
 
    </>
   )
