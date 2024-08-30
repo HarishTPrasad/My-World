1daa1948a91fe36b5c30cbaec7b95cc38fc13206
Harry <harish1090prasad@gmail.com>
2024-08-30 07:23:27 +0530

    new updates

--- src/components/Resume/Resume.jsx
+++ src/components/Resume/Resume.jsx
@@ -9,61 +9,61 @@ export default function Resume() {
   return (
     <>
 
-<section id="resume" class="section">
-      <div class="sectionContainer" >
+            <section id="resume" class="section">
+                  <div class="sectionContainer" >
 
 
-        <section id="resume" class="custom-section">
-          <div class="custom-section-container">
-            <h1 class="download"> <i class="fa-solid fa-download"></i> Download My Resume</h1>
-            <div class="action__btns">
-              <button onclick="downloadPDF()" class="downloadbt"><i class="fa-solid fa-file"></i> Resume</button>
-            </div>
-          </div>
-        </section>
+                    <section id="resume" class="custom-section">
+                      <div class="custom-section-container">
+                        <h1 class="download"> <i class="fa-solid fa-download"></i> Download My Resume</h1>
+                        <div class="action__btns">
+                          <button onclick="downloadPDF()" class="downloadbt"><i class="fa-solid fa-file"></i> Resume</button>
+                        </div>
+                      </div>
+                    </section>
 
-         
-        <div class="project-section" >
-          <h1 class="myprojecth" ><i class="fa-solid fa-award fa-bounce"></i> My Certifications !!!!</h1>
-          <p class="myprojectp"  > Hey! have a look at my little achivements. </p>
-      
-          <div class="myprojectcard">
+                    
+                    <div class="project-section" >
+                      <h1 class="myprojecth" ><i class="fa-solid fa-award fa-bounce"></i> My Certifications !!!!</h1>
+                      <p class="myprojectp"  > Hey! have a look at my little achivements. </p>
+                  
+                      <div class="myprojectcard">
 
-              <div class="project-card">
-                  <h3 class="project-title">ISRO Certification on Remote Sensing and digital Image Analysis</h3>
+                          <div class="project-card">
+                              <h3 class="project-title">ISRO Certification on Remote Sensing and digital Image Analysis</h3>
 
-                  <img src={myImage} alt="Project Image"/>
-
-
-                  <a href="#" class="project-link"><i class="fa-solid fa-eye"></i> View</a>
-              </div>
-      
-            <div class="project-card">
-                  <h3 class="project-title">Google ads display Certification by Google </h3>
-
-                  <img src={myImagea} alt="Project Image"/>
+                              <img src={myImage} alt="Project Image"/>
 
 
-                  <a href="#" class="project-link"><i class="fa-solid fa-eye"></i> View</a>
-              </div>
-      
-              <div class="project-card">
-                  <h3 class="project-title">Galactic Prablem Solver By NASA and 12+ space Agencies</h3>
+                              <a href="#" class="project-link"><i class="fa-solid fa-eye"></i> View</a>
+                          </div>
+                  
+                        <div class="project-card">
+                              <h3 class="project-title">Google ads display Certification by Google </h3>
 
-                  <img src={myImagee} alt="Project Image"/>
+                              <img src={myImagea} alt="Project Image"/>
 
 
-                  <a href="#" class="project-link"><i class="fa-solid fa-eye"></i> View</a>
-              </div>
-          </div>
+                              <a href="#" class="project-link"><i class="fa-solid fa-eye"></i> View</a>
+                          </div>
+                  
+                          <div class="project-card">
+                              <h3 class="project-title">Galactic Prablem Solver By NASA and 12+ space Agencies</h3>
 
-          <div>
-            <h3 class="space"> lot's of more Certifications on my Linkedin account.</h3>
-          </div>
-      </div>
+                              <img src={myImagee} alt="Project Image"/>
 
-      </div>
-  </section>
+
+                              <a href="#" class="project-link"><i class="fa-solid fa-eye"></i> View</a>
+                          </div>
+                      </div>
+
+                      <div>
+                        <h3 class="space"> lot's of more Certifications on my Linkedin account.</h3>
+                      </div>
+                  </div>
+
+                  </div>
+              </section>
 
     
     </>
--- src/components/Team/Team.jsx
+++ src/components/Team/Team.jsx
@@ -22,37 +22,37 @@ export default function Team() {
 
     <>
 
-    
-<section id="team" class="section">
-    <div class="section__container" >
-      <div class="image"  >
-        <img src={myImage} alt="profile" />
-      </div>
-      <div class="content">
-        <p class="subtitle">{text}</p>
-        <h1 class="title">
-          We are <span><i class="fa-solid fa-earth-asia fa-spin"></i> CYPHER ELITES<br />the</span> Tech Freaks
-        </h1>
-        <p class="description"> <i class="fa-solid fa-code"></i>
-          Welcome to the Cypher Elites – your ticket to the future of space exploration! 🚀<br/><br/>
+            
+        <section id="team" class="section">
+            <div class="section__container" >
+              <div class="image"  >
+                <img src={myImage} alt="profile" />
+              </div>
+              <div class="content">
+                <p class="subtitle">{text}</p>
+                <h1 class="title">
+                  We are <span><i class="fa-solid fa-earth-asia fa-spin"></i> CYPHER ELITES<br />the</span> Tech Freaks
+                </h1>
+                <p class="description"> <i class="fa-solid fa-code"></i>
+                  Welcome to the Cypher Elites – your ticket to the future of space exploration! 🚀<br/><br/>
 
-          Join us in crafting a revolutionary website or software, the Planetary Tourism Office, as we embark on an exciting journey to make the universe accessible to all. Whether you're a coder, designer, or simply a space enthusiast, your unique talents can help us redefine how we explore the cosmos.<br/><br/>
-          
-          Let's create history together and reach for the stars! 🌌🌟 
-        </p>
-        <div class="action__btns">
+                  Join us in crafting a revolutionary website or software, the Planetary Tourism Office, as we embark on an exciting journey to make the universe accessible to all. Whether you're a coder, designer, or simply a space enthusiast, your unique talents can help us redefine how we explore the cosmos.<br/><br/>
+                  
+                  Let's create history together and reach for the stars! 🌌🌟 
+                </p>
+                <div class="action__btns">
 
-          <a href="https://chat.whatsapp.com/J3eguflen8L62OtgXBHQC4" rel="noopener noreferrer">
-            <button class="hire__me"><i class="fa-solid fa-people-group fa-fade"></i> Join Us</button>
-          </a>
-          <a href="https://chat.whatsapp.com/J3eguflen8L62OtgXBHQC4" rel="noopener noreferrer">
-             <button class="portfolio"><i class="fa-solid fa-microchip"></i> Cypher Elites</button>
-           </a>
-        </div>
-      </div>
-     
-    </div>
-  </section>
+                  <a href="https://chat.whatsapp.com/J3eguflen8L62OtgXBHQC4" rel="noopener noreferrer">
+                    <button class="hire__me"><i class="fa-solid fa-people-group fa-fade"></i> Join Us</button>
+                  </a>
+                  <a href="https://chat.whatsapp.com/J3eguflen8L62OtgXBHQC4" rel="noopener noreferrer">
+                    <button class="portfolio"><i class="fa-solid fa-microchip"></i> Cypher Elites</button>
+                  </a>
+                </div>
+              </div>
+            
+            </div>
+          </section>
     
     
     </>
