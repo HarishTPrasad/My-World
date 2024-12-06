5f6debefcca2d770a607f0374e8764258be5965e
Harry <harish1090prasad@gmail.com>
2024-12-06 22:13:10 +0530

    new updates

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -4,54 +4,54 @@ export default function Contact() {
   return (
     <>
 
-            <section id="contact" class="section">
-              <div class="sectionContainer" >
+            <section id="contact" className="section">
+              <div className="sectionContainer" >
 
-                <div class="contactdiv1">
-                  <h2 class="contacth2">Contact Me</h2>
-                  <h3 class="contacth2"> <i class="fa-solid fa-envelope"></i>  Email : <span></span>harish1090prasad@gmail.com </h3>
-                  <h3 class="contacth2"> <i class="fa-solid fa-phone"></i>  Contact no: <span></span> 7600922448 </h3>
+                <div className="contactdiv1">
+                  <h2 className="contacth2">Contact Me</h2>
+                  <h3 className="contacth2"> <i className="fa-solid fa-envelope"></i>  Email : <span></span>harish1090prasad@gmail.com </h3>
+                  <h3 className="contacth2"> <i className="fa-solid fa-phone"></i>  Contact no: <span></span> 7600922448 </h3>
                 </div>
 
-                <div class="ending">
+                <div className="ending">
 
 
-                  <div class="mybtns" >
+                  <div className="mybtns" >
 
                     <a href="https://github.com/HarishTPrasad" >
-                      <button  class="mylinks"><i class="fa-brands fa-github fa-xl"></i> GITHUB</button>
+                      <button  className="mylinks"><i className="fa-brands fa-github fa-xl"></i> GITHUB</button>
                       </a>
 
                       <a href="https://www.linkedin.com/in/harish-prasad-75a680243/" >
-                        <button class="mylinks"><i class="fa-brands fa-linkedin-in fa-xl"></i> LINKEDIN</button>
+                        <button className="mylinks"><i className="fa-brands fa-linkedin-in fa-xl"></i> LINKEDIN</button>
                       </a>
 
                       <a href="https://www.instagram.com/harryyy._13/" >
-                        <button class="mylinks"><i class="fa-brands fa-instagram fa-xl"></i> INSTAGRAM</button>
+                        <button className="mylinks"><i className="fa-brands fa-instagram fa-xl"></i> INSTAGRAM</button>
                       </a>
 
                       <a href="https://wa.me/qr/IKL3YFWY4TZLP1" >
-                        <button class="mylinks"><i class="fa-brands fa-whatsapp fa-xl"></i> WHATS APP</button>
+                        <button className="mylinks"><i className="fa-brands fa-whatsapp fa-xl"></i> WHATS APP</button>
                       </a>
 
                   </div>
 
-                  <div class="contact-content">
+                  <div className="contact-content">
                     
-                    <p class="section-description">Feel free to reach out. I'd love to connect ! <i class="fa-regular fa-paper-plane"></i></p>
+                    <p className="section-description">Feel free to reach out. I'd love to connect ! <i className="fa-regular fa-paper-plane"></i></p>
 
-                    <form action="#" method="post" class="contact-form">
-                        <label for="name"><i class="fa-solid fa-pen"></i> Your Name:</label>
-                        <input type="text" id="name" name="name" class="contact-input" required />
+                    <form className="contact-form">
+                        <label for="name"><i className="fa-solid fa-pen"></i> Your Name:</label>
+                        <input type="text" id="name" name="name" className="contact-input" required />
 
-                        <label for="email"><i class="fa-solid fa-pen"></i> Your Email:</label>
-                        <input type="email" id="email" name="email" class="contact-input" required />
+                        <label for="email"><i className="fa-solid fa-pen"></i> Your Email:</label>
+                        <input type="email" id="email" name="email" className="contact-input" required />
 
-                        <label for="message"><i class="fa-solid fa-pen"></i> Your Message:</label>
-                        <textarea id="message" name="message" rows="4" class="contact-input" required></textarea>
+                        <label for="message"><i className="fa-solid fa-pen"></i> Your Message:</label>
+                        <textarea id="message" name="message" rows="4" className="contact-input" required></textarea>
 
-                      <div class="mysubmit">
-                        <input type="submit" value="Submit" class="contact-submit"/>
+                      <div className="mysubmit">
+                        <input type="submit" value="Submit" className="contact-submit"/>
                       </div>
                     </form>
 
